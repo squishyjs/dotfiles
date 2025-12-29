dd391fe2982ba1254dd9c925575370a276518123
john <johnsciuto3@gmail.com>
2025-12-29 11:41:34 +1100

    Add Git Graph & Lazy Lock

--- nvim/init.lua
+++ nvim/init.lua
@@ -336,6 +336,27 @@ require("lazy").setup({
 		config = true,
 	},
 
+	-- GIT-GRAPH
+	{
+		"isakbm/gitgraph.nvim",
+		dependencies = { "sindrets/diffview.nvim" },
+		---@type I.GGConfig
+		opts = {
+			hooks = {
+				-- Check diff of a commit
+				on_select_commit = function(commit)
+					vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+					vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+				end,
+				-- Check diff from commit a -> commit b
+				on_select_range_commit = function(from, to)
+					vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+					vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+				end,
+			},
+		},
+	},
+
 	-- ALPHA NVIM (Dashboard)
 	{
 		"goolord/alpha-nvim",
