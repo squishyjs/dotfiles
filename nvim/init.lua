0b820f61853a886b73e89de00f6a399fcac339ef
john <johnsciuto3@gmail.com>
2025-12-29 11:43:43 +1100

    Add Git Graph

--- nvim/init.lua
+++ nvim/init.lua
@@ -339,6 +339,38 @@ require("lazy").setup({
 	-- GIT-GRAPH
 	{
 		"isakbm/gitgraph.nvim",
+		opts = {
+			git_cmd = "git",
+			symbols = {
+				merge_commit = "M",
+				commit = "*",
+			},
+			format = {
+				timestamp = "%H:%M:%S %d-%m-%Y",
+				fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+			},
+			hooks = {
+				on_select_commit = function(commit)
+					print("selected commit:", commit.hash)
+				end,
+				on_select_range_commit = function(from, to)
+					print("selected range:", from.hash, to.hash)
+				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>gl",
+				function()
+					require("gitgraph").draw({}, { all = true, max_count = 5000 })
+				end,
+				desc = "GitGraph - Draw",
+			},
+		},
+	},
+	-- View commit with Diffview.nvim
+	{
+		"isakbm/gitgraph.nvim",
 		dependencies = { "sindrets/diffview.nvim" },
 		---@type I.GGConfig
 		opts = {
