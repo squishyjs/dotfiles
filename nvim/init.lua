afb1267ddb03efcba6cbd3fb95703e808df7d6bb
john <johnsciuto3@gmail.com>
2026-01-15 10:54:05 +1100

    Modify init.lua
    Turn off "NormalFloat" bg transparency -> allow colour for floats

--- nvim/init.lua
+++ nvim/init.lua
@@ -2351,9 +2351,8 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
--- Transparency
+-- Background Transparency
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
